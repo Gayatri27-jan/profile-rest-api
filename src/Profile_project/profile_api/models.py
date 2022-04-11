2c275788a7dee8f8a6c55f988c170eed56f574d7
gayatri <gayatriwanjari27@gmail.com>
2022-04-11 09:13:55 +0530

    Added a custom user profile model,  a manager , and the migration for our database.

--- src/Profile_project/profile_api/models.py
+++ src/Profile_project/profile_api/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    Represent a "user Profile " inside the system
+    """
+
+    email = models.EmailField(max_length=225, unique=True)
+    name = models.CharField(max_length=225)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = 'name'
+
+    def get_username(self):
+        """
+        use to get username
+        :return: name
+        """
+        return self.name
+
+    def get_short_name(self):
+        """
+        usef to get short name of user
+        :return: name
+        """
+        return self.name
+
+    def __str__(self):
+        """
+        django uses this when it needs to convert the object to a strings
+        :return: str object
+        """
+        return self.email
